1872a4a115a69c981577cff8efbc5efa0bf4a1eb
Xarwex <60325364+Xarwex@users.noreply.github.com>
2026-02-28 18:45:46 +0000

    flake wip

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    esp32 = {
+      url = "github:leighleighleigh/esp-rs-nix/6be9cd080962efa09cac2bbf807efdedafa62269";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      esp32,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -21,8 +26,30 @@
       in
       {
         devShells.default = pkgs.mkShell {
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          RUSTUP_TOOLCHAIN = "${esp32.packages.${system}.default}";
+          RUST_SRC_PATH = "${esp32.packages.${system}.default}/lib/rustlib/src/rust/library";
+
+          inputsFrom = [
+            esp32.devShells.${system}.default
+          ];
+
+          buildInputs = [
+            pkgs.rustup
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.zlib ]}:$LD_LIBRARY_PATH"
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+          '';
+
           packages = [
-            pkgs.devcontainer
+            pkgs.rustup
+            pkgs.rust-analyzer
+            # pkgs.clang
+            #pkgs.rust-analyzer
+            #esp-generate
+            #cargo-feature
           ];
         };
       }
