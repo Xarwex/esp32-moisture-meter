c24a79461b5c4e8297ee44a097f066f1f9321b30
Xarwex <60325364+Xarwex@users.noreply.github.com>
2026-04-24 18:50:04 +0100

    modify adc range

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,24 @@ use core::fmt::Write;
 use constcat::concat;
 use embassy_executor::Spawner;
 use embassy_net::{
-    DhcpConfig, Runner, Stack, StackResources,
+    DhcpConfig, Runner, StackResources,
     dns::DnsSocket,
     tcp::client::{TcpClient, TcpClientState},
 };
-use embassy_time::{Duration, Timer, with_timeout};
+use embassy_time::Timer;
 
 // Installing global alloc + panic/backtrace handlers by importing for side effects.
 use esp_alloc as _;
 use esp_backtrace as _;
 
 use esp_hal::{
-    Blocking,
-    analog::adc::{Adc, AdcConfig, AdcPin, Attenuation},
+    analog::adc::{Adc, AdcConfig, Attenuation},
     clock::CpuClock,
-    peripherals::{ADC1, ADC2, GPIO15, GPIO34},
-    ram,
     rng::Rng,
-    rtc_cntl::{Rtc, sleep::TimerWakeupSource, wakeup_cause},
     timer::timg::TimerGroup,
 };
-use esp_println::{dbg, println};
-use esp_radio::wifi::{ClientConfig, Config, ModeConfig, ScanConfig, WifiController, WifiDevice};
+use esp_println::println;
+use esp_radio::wifi::{ClientConfig, ModeConfig, WifiDevice};
 use heapless::String;
 // Lightweight HTTP client for embedded targets.
 use reqwless::{
@@ -44,22 +40,19 @@ macro_rules! mk_static {
     ($t:ty,$val:expr) => {{
         static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
         #[deny(unused_attributes)]
-        let x = STATIC_CELL.uninit().write(($val));
+        let x = STATIC_CELL.uninit().write($val);
         x
     }};
 }
-// Deep-sleep interval (1 hour) for low battery drain.
-const WAKE_INTERVAL_SECS: u64 = 10;
-// This is maximum ADC that is achieved when the sensor is connected.
-const ADC_MAX: u16 = 3300;
-// Maximum wait time to get DHCP lease after Wi-Fi link is up.
-const DHCP_TIMEOUT_SECS: u64 = 30;
-// Maximum wait time to connect station to AP.
-const WIFI_CONNECT_TIMEOUT_SECS: u64 = 30;
-// Maximum wait time for one HTTP webhook request.
-const REQUEST_TIMEOUT_SECS: u64 = 60;
-// Retry count per wake cycle to avoid infinite battery drain.
-const MAX_PUBLISH_RETRIES: u8 = 3;
+// Deep-sleep interval between webhook updates.
+const WAKE_INTERVAL_SECS: u64 = 60 * 60 * 3; // 3 hours
+// Once we're connected we don't really disconnect, so we can spam a bit. This will only go wrong if
+// the WiFI goes out.
+const WIFI_RETRY_SECS: u64 = 10;
+// Calibration points from measured readings on the previous scale:
+// 10% was fully wet and 60% was fully dry.
+const WET_ADC: u16 = 2970;
+const DRY_ADC: u16 = 1320;
 
 const BUFFER_SIZE: usize = 4096;
 
@@ -94,7 +87,7 @@ async fn main(spawner: Spawner) -> ! {
 
     println!("Initializing wifi controller");
     let (mut wifi_controller, interfaces) =
-        esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
+        esp_radio::wifi::new(radio_init, peripherals.WIFI, Default::default())
             .expect("Failed to initialize Wi-Fi controller");
 
     let net_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
@@ -142,9 +135,9 @@ async fn main(spawner: Spawner) -> ! {
             println!("Connecting to {WIFI_SSID}...");
             if let Err(e) = wifi_controller.connect_async().await {
                 println!(
-                    "Couldn't connect to {WIFI_SSID}, because {e}, retrying in {WAKE_INTERVAL_SECS}s"
+                    "Couldn't connect to {WIFI_SSID}, because {e}, retrying in {WIFI_RETRY_SECS}s"
                 );
-                Timer::after_secs(WAKE_INTERVAL_SECS).await;
+                Timer::after_secs(WIFI_RETRY_SECS).await;
                 continue;
             }
             println!("Connected to {WIFI_SSID}")
@@ -159,6 +152,7 @@ async fn main(spawner: Spawner) -> ! {
 
         let moisture_percent = {
             let raw_adc = nb::block!(adc.read_oneshot(&mut pin)).unwrap();
+            println!("Read raw adc {raw_adc}");
             moisture_percent_from_adc(raw_adc)
         };
         body.clear();
@@ -177,11 +171,6 @@ async fn main(spawner: Spawner) -> ! {
             Err(e) => println!("Got error {e:?} when trying to construct the http request"),
         }
 
-        // let mut http_req = client
-        //     .request(Method::POST, WEBHOOK_URL)
-        //     .await
-        //     .unwrap()
-        //     .body(body.as_bytes());
         Timer::after_secs(WAKE_INTERVAL_SECS).await;
     }
 }
@@ -191,21 +180,11 @@ async fn net_task(mut runner: Runner<'static, WifiDevice<'static>>) {
     runner.run().await;
 }
 
-// #[embassy_executor::task]
-// async fn moisture_reporting(
-//     mut controller: WifiController<'static>,
-//     stack: Stack<'static>,
-//     mut pin: AdcPin<GPIO34<'static>, ADC1<'static>>,
-//     mut adc: Adc<'static, ADC1<'static>, Blocking>,
-// ) {
-// }
-//
-// 2269 is dry -> set 2500 as baseline dry
-// 1232 is wet -> set 1000 as max wet
-// Given any reading - clamp it between these two values first, then the actual percentage of
-// wetness is 100 - ((clamp(x) - 1000) * 100 / 1500)
 // Convert raw ADC sample to integer percentage.
 pub fn moisture_percent_from_adc(raw_adc: u16) -> u8 {
-    let reversed_raw_adc = (ADC_MAX as u32).saturating_sub(raw_adc as u32) * 100;
-    (reversed_raw_adc / ADC_MAX as u32) as u8
+    let clamped_adc = raw_adc.clamp(DRY_ADC, WET_ADC);
+    let dry_adc = u32::from(DRY_ADC);
+    let wet_adc = u32::from(WET_ADC);
+
+    (((u32::from(clamped_adc) - dry_adc) * 100) / (wet_adc - dry_adc)) as u8
 }
